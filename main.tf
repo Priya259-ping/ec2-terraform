5f0babcb2fc4abef267858ab56e1a9ca4d775313
Priya259 <priya1420241@gmail.com>
2024-10-18 23:56:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"  # Set your region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "one" {
   ami           = "ami-06b21ccaeff8cd686"  # Example AMI ID, change as per region
   instance_type = "t2.micro"
   
   tags = {
-    Name = "MyEC2Instance"
+    Name = "terraform-server"
   }
 }

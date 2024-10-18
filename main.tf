5f1e0f7ce3fa058d3d18a76cd5cfe3a84a186e1d
Priya259 <priya1420241@gmail.com>
2024-10-18 23:01:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"  # Set your region
+  region = "us-east-1"  # Set your region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI ID, change as per region
+  ami           = "ami-06b21ccaeff8cd686"  # Example AMI ID, change as per region
   instance_type = "t2.micro"
   
   tags = {
